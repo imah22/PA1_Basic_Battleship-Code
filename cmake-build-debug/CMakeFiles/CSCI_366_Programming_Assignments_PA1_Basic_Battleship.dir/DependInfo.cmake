27c8979988ed7741e60b7a6f0c6829956adb06f6
Ibrahim Alsaleh <35679543+imah22@users.noreply.github.com>
2020-03-13 23:02:21 -0600

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Ibrahim/Desktop/Computer Systems/CSCI_366_Programming_Assignments-PA1_Basic_Battleship/src/Client.cpp" "/mnt/c/Users/Ibrahim/Desktop/Computer Systems/CSCI_366_Programming_Assignments-PA1_Basic_Battleship/cmake-build-debug/CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/src/Client.cpp.o"
+  "/mnt/c/Users/Ibrahim/Desktop/Computer Systems/CSCI_366_Programming_Assignments-PA1_Basic_Battleship/src/Server.cpp" "/mnt/c/Users/Ibrahim/Desktop/Computer Systems/CSCI_366_Programming_Assignments-PA1_Basic_Battleship/cmake-build-debug/CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/src/Server.cpp.o"
+  "/mnt/c/Users/Ibrahim/Desktop/Computer Systems/CSCI_366_Programming_Assignments-PA1_Basic_Battleship/src/client_main.cpp" "/mnt/c/Users/Ibrahim/Desktop/Computer Systems/CSCI_366_Programming_Assignments-PA1_Basic_Battleship/cmake-build-debug/CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/src/client_main.cpp.o"
+  "/mnt/c/Users/Ibrahim/Desktop/Computer Systems/CSCI_366_Programming_Assignments-PA1_Basic_Battleship/src/server_main.cpp" "/mnt/c/Users/Ibrahim/Desktop/Computer Systems/CSCI_366_Programming_Assignments-PA1_Basic_Battleship/cmake-build-debug/CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/src/server_main.cpp.o"
+  "/mnt/c/Users/Ibrahim/Desktop/Computer Systems/CSCI_366_Programming_Assignments-PA1_Basic_Battleship/test/tests.cpp" "/mnt/c/Users/Ibrahim/Desktop/Computer Systems/CSCI_366_Programming_Assignments-PA1_Basic_Battleship/cmake-build-debug/CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/test/tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/src/Client.cpp.o"
+  "CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/src/Server.cpp.o"
+  "CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/src/client_main.cpp.o"
+  "CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/src/server_main.cpp.o"
+  "CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/test/tests.cpp.o"
+  "CSCI_366_Programming_Assignments_PA1_Basic_Battleship"
+  "CSCI_366_Programming_Assignments_PA1_Basic_Battleship.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSCI_366_Programming_Assignments_PA1_Basic_Battleship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
